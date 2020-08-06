6a14fb76f413e7db2728beaf9f6fd2e4fba93b42
Frank Josh Olloqui <josh.olloqui@gmail.com>
2020-08-05 18:10:39 -0700

    add css styling and magic

--- src/Search.js
+++ src/Search.js
@@ -4,8 +4,11 @@ class Search extends React.Component {
     render() { 
         return (
             <div>
-                <input onChange={this.props.search} />
-                <button onClick={this.props.click}>Find</button>
+                <h2>Search the Pokedex</h2>
+                <div className="searchBar">
+                    <input onChange={this.props.search} placeholder='Enter Pokemon Name...'/>
+                    <button onClick={this.props.click}><img className="search" alt='search' src='search.png'/></button>
+                </div>
             </div>
         );
     }
